cfcb444c802e421640e5d07cc27936fbd2667fca
jackmford <jackmitchellfordyce@gmail.com>
2022-03-05 12:02:32 -0600

    trying to remember how to work with javascript requests

--- server.go
+++ server.go
@@ -24,14 +24,21 @@ func hello(w http.ResponseWriter, r *http.Request) {
     fmt.Println(records[0][0])
     data := struct {
         Id string
+        Text string
     }{
         Id: records[0][0],
+        Text: records[1][0],
     }
     tpl.Execute(w, data)
 }
 
+func writeIdData(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("test")
+}
+
 func main() {
     http.HandleFunc("/", hello)
+    http.HandleFunc("/write", writeIdData)
     http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
     err := http.ListenAndServe(":8787", nil)
     if err != nil {
--- static/index.js
+++ static/index.js
@@ -2,10 +2,16 @@ function changeColor() {
   if (document.getElementById("redbutton") != null){
     document.getElementById("redbutton").innerHTML = "Clean";
     document.getElementById("redbutton").id = "bluebutton";
+    let btn-color = "bluebutton";
   }
   else{
     document.getElementById("bluebutton").innerHTML = "Dirty";
     document.getElementById("bluebutton").id = "redbutton";
+    let btn-color = "redbutton";
   }
+
+  var data = new FormData();
+  data.append("color", btn-color);
+  fetch("localhost:8787/write", { method: "POST", body: data})
 }
 
